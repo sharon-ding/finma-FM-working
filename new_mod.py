e735362fa017c356fa7cf29dc816a36a1eeae93a
sharon-ding <sharon.ding@finma.au>
2026-01-22 22:58:37 +0000

    enhanced hello world

--- new_mod.py
+++ new_mod.py
@@ -1,3 +1,3 @@
-def hello_world(adjective):
-    print(f'Hello {adjective} world!')
-    
+def hello_world(adjective1, adjective2):
+    print(f'Hello {adjective1} world!')
+    print(f'What an {adjective2} day!')
